91a7e3615b4b18c0429c92a4ae129580719ae947
Matti Jokitulppo <melonmanchan@gmail.com>
2017-09-03 20:35:00 +0300

    Less timeout for CTRL

--- keyboards/hhkb/config.h
+++ keyboards/hhkb/config.h
@@ -36,7 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 #define MATRIX_COLS 8
 
-#define TAPPING_TERM    200
 
 /* number of backlight levels */
 #define BACKLIGHT_LEVELS 3
@@ -72,5 +71,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 #define PERMISSIVE_HOLD
+#define TAPPING_TERM 100 // how long before a tap becomes a hold
 
 #endif
